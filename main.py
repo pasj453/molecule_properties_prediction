5d67d05c69314ec63b6e342c0aa25055520bb975
jgrec <jgrec.bio@gmail.com>
2020-11-03 21:26:44 +0100

    Trains with dummy classifier

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import os
+import json
+import pickle
 import argparse
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.dummy import DummyClassifier
+
+from vectorization import load_dataset, vectorizes_features, vectorizes_label
 
 
 def check_argument_parsing(args: argparse.Namespace) -> argparse.Namespace:
@@ -34,7 +42,12 @@ def get_parser() -> argparse.ArgumentParser():
     parser.add_argument("--fname", help="filepath for dataset")
     parser.add_argument("--model", help="filepath for model loading or saving",
                         default="models/model.h5")
+    parser.add_argument("--model-type", choices=["dummy"], default="dummy")
+    parser.add_argument("--output-dir", default="outputs")
     parser.add_argument("--smile")
+    parser.add_argument("--random-state", type=int,
+                        help="integer value to seed the random generator",
+                        default=1729)
     return parser
 
 
@@ -42,3 +55,34 @@ if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
     checked_args = check_argument_parsing(args)
+
+    if checked_args.task == "train":
+        df = load_dataset(args.fname)
+        x, y = vectorizes_features(df), vectorizes_label(df)
+
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y,
+            test_size=0.9,
+            random_state=args.random_state
+        )
+
+        if not os.path.isdir(args.outputs):
+            os.mkdir(args.outputs)
+        for arr in (x_train, x_test, y_train, y_test):
+            np.save(os.path.join(args.outputs, arr.__name__), arr)
+
+        if args.model_type == "dummy":
+            clf = DummyClassifier(strategy="most_frequent")
+            clf.fit(x_train, y_train)
+            score = clf.score(x_test, y_test)
+
+            with open(args.model, "wb") as f:
+                pickle.dump(clf, f)
+
+        res = {
+            "model-type": args.model_type,
+            "random_state": args.random_state,
+            "score": score
+        }
+        with open(os.path.join(args.outputs, "resultats.json"), "w") as f:
+            json.dump(res, f)
